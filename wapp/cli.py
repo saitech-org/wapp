3b2441eca5e8a02fd8080bee1ba4344a029e65ac
bob-the-coder <its.bobs.workmail@gmail.com>
2025-09-13 23:21:38 +0300

    real bootstrapping

--- wapp/cli.py
+++ wapp/cli.py
@@ -1,5 +1,8 @@
 import os
+import sys
+
 import click
+import subprocess
 import importlib.resources as pkg_resources
 
 TEMPLATE_FILES = [
@@ -9,13 +12,26 @@ TEMPLATE_FILES = [
     'create_app.py',
 ]
 
+DEPENDENCIES = [
+    'saitech-wapp',
+    'flask',
+    'flask_sqlalchemy',
+    'alembic',
+    'flasgger',
+    'python-dotenv',
+    'pydantic',
+]
+
 
 @click.command()
 @click.argument('project_name')
 def wapp_init(project_name):
     """Bootstrap a new Wapp project."""
     try:
+        # Create project directory
         os.makedirs(project_name)
+
+        # Create project files from templates
         for filename in TEMPLATE_FILES:
             try:
                 with pkg_resources.files('wapp.templates').joinpath(filename).open('r', encoding='utf-8') as src_file:
@@ -24,7 +40,18 @@ def wapp_init(project_name):
                     dest_file.write(content)
             except Exception as e:
                 click.echo(f'Error copying {filename}: {e}')
+
+        # Install necessary dependencies
+        subprocess.run([sys.executable, "-m", "pip", "install"] + DEPENDENCIES, check=True)
+
+        # Change into the project directory
+        os.chdir(project_name)
+
+        # Initialize Alembic
+        subprocess.run([sys.executable, "-m", "alembic", "init", "migrations"], check=True)
+
         click.echo(f'Project {project_name} initialized successfully!')
+
     except Exception as e:
         click.echo(f'Error initializing project: {e}')
 
